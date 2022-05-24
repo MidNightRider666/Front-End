25d9ef40ee3a56a658ec41b2cf05ff632ea56264
MidNightRider666 <ledas850@gmail.com>
2022-05-24 21:35:48 +0300

    Auth creation, fixing README

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import './App.css';
+import AuthContext from './components/store/AuthContx';
 
 function App() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  function login() {
+    setIsUserLoggedIn(true);
+  }
+  function logout() {
+    setIsUserLoggedIn(false);
+  }
+
+  const ctxValue = {
+    isUserLoggedIn,
+    login,
+    logout,
+  };
   return (
+    <AuthContext.Provider value={ctxValue}>
     <div className="App">
       Apps
     </div>
+    </AuthContext.Provider>
   );
 }
 
